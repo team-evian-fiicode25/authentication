9e9482c06735c550af11f42099759fab8aaf7916
atomicul <soaremihai2006@gmail.com>
2025-03-01 16:04:20 +0200

    Fix db tracking of Login helper

---
+++ API/Types/Helper/Login.cs
@@ -0,0 +1,18 @@
+using Fiicode25Auth.API.Types.Helper.Abstract;
+
+namespace Fiicode25Auth.API.Types.Helper;
+
+public class Login : ILogin
+{
+    public Guid? Id {get; private set;}
+    public string? Username {get; set;}
+    public IPassword Password {get; set;}
+    public IPhoneNumber? PhoneNumber {get; set;}
+    public IEmail? Email {get; set;}
+
+    public Login(IPassword password, Guid? id = null)
+    {
+        Id=id;
+        Password=password;
+    }
+}
