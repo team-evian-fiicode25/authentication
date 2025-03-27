44cd95d6ad29a4a1c3858ad57c9cf06b8150c7a3
atomicul <soaremihai2006@gmail.com>
2025-03-27 18:18:11 +0200

    Enforce password security

--- API/Types/Helper/Login/Password.cs
+++ API/Types/Helper/Login/Password.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+using Fiicode25Auth.API.Exceptions;
 using Fiicode25Auth.API.Types.Helper.Login.Abstract;
 
 namespace Fiicode25Auth.API.Types.Helper.Login;
@@ -12,5 +14,59 @@ public class Password : IPassword
     public Password(string passwordHash) => Hash=passwordHash;
 
     public static Password FromPassword(string password)
-        => new(BCrypt.Net.BCrypt.HashPassword(password));
+    {
+        _validatePassword(password);
+
+        return new(BCrypt.Net.BCrypt.HashPassword(password));
+    }
+
+    private static void _validatePassword(string password)
+    {
+        if (password.Length < 12)
+        {
+            throw new PasswordFormatException("Passwords must be at least 12 characters long");
+        }
+
+        if (Regex.IsMatch(password, @"\s"))
+        {
+            throw new PasswordFormatException("Passwords must not contain any whitespace");
+        }
+
+        var lowercaseChars = Enumerable
+            .Range('a', 'z' - 'a' + 1)
+            .Select(ch => (char)ch);
+
+        if (!password.Any(ch => lowercaseChars.Contains(ch)))
+        {
+            throw new PasswordFormatException("Passwords should contain at least one lowercase character");
+        }
+
+        var uppercaseChars = Enumerable
+            .Range('A', 'Z' - 'A' + 1)
+            .Select(ch => (char)ch);
+
+        if (!password.Any(ch => uppercaseChars.Contains(ch)))
+        {
+            throw new PasswordFormatException("Passwords should contain at least one uppercase character");
+        }
+
+        var numericalChars = Enumerable
+            .Range('0', 10)
+            .Select(ch => (char)ch);
+
+        if (!password.Any(ch => numericalChars.Contains(ch)))
+        {
+            throw new PasswordFormatException("Passwords should contain at least one number");
+        }
+
+
+        if (!password.Any(ch =>
+                    !numericalChars.Contains(ch) &&
+                    !lowercaseChars.Contains(ch) &&
+                    !uppercaseChars.Contains(ch)))
+        {
+            throw new PasswordFormatException("Passwords should contain at least one special character");
+        }
+
+    }
 }
